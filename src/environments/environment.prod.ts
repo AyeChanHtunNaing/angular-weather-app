c8585ec363253d742e69a749fb4647ecc672272f
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2022-10-03 07:28:51 +0630

    added heroku deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl:'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'33cfdc0999mshae7ddc7f1c84010p12acc0jsn6eefe1a60dab'
+
 };
